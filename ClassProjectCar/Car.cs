bbad041a7921c64bb1eb567308fcf2d7b81463e0
Mona Lisa <email@example.com>
2020-09-01 11:17:05 -0600

    starting

---
+++ ClassProjectCar/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ClassProjectCar
+{
+    public class Car
+    {
+        public string Make { set; get; }
+        public string Model { set; get; }
+        public double Year { set; get; }
+
+        public void WriteOutCar()
+        {
+            Console.WriteLine($"Make: {Make}, Model: {Model}, Year: {Year}");
+        }
+        
+    }
+}
---
+++ ClassProjectCar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassProjectCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            var Car1 = new Car();
+            Car1.Make = "Honda";
+            Car1.Model = "Civic";
+            Car1.Year = 2017;
+
+            var Car2 = new Car();
+            Car2.Make = "Dodge";
+            Car2.Model = "Ram";
+            Car2.Year = 1980;
+
+            var AllCars = new List<Car>();
+
+            AllCars.Add(Car1);
+            AllCars.Add(Car2);
+
+            foreach (var eachCar in AllCars)
+            {
+                Console.WriteLine("---------------");
+                Console.WriteLine($" Make: {eachCar.Make}");
+                Console.WriteLine($" Model: {eachCar.Model}");
+                Console.WriteLine($" Year: {eachCar.Year}");
+
+                //Console.WriteLine($"Make: {Car1.Make}, Model: {Model}, Year: {Year}");
+            }
+
+        }
+    }
+}
